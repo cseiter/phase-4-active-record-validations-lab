f6d4dc402060994451ffd1c27d4385c7fe4b3848
chris <cseiter@gmail.com>
2021-08-31 21:42:53 -0400

    custom valication completed, all tests pass

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,19 @@
 class Post < ApplicationRecord
-    validates :title, presence:true
+    validates :title, presence: true
     validates :content, length: {minimum: 250}
     validates :summary, length: {maximum: 250}
     validates :category, inclusion: {in: %w(Fiction Non-Fiction)}
+    validate :clickbait?
+
+    REG_PATTERN = [
+        /Won't Believe/i,
+        /Secret/i,
+        /Top \d/i,
+        /Guess/i
+      ]
+    def clickbait?
+        if REG_PATTERN.none? {|pat| pat.match title}
+            errors.add(:title, "must be clickbait")
+        end  
+    end
 end
